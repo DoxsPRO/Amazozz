a8a3d19b7066c0562534caf6192298f3d524caf6
DoxsPRO <doxspro1@gmail.com>
2020-05-13 11:04:50 +0200

    Privacy

--- indexLog.php
+++ indexLog.php
@@ -150,6 +150,8 @@
       <p><a href="#" title="Link">Link 2</a></p>
       <p><a href="#" title="Link">Link 3</a></p>
     </div>
+	  	<div><a href="https://www.iubenda.com/privacy-policy/13672304" class="iubenda-black iubenda-embed" title="Privacy Policy ">Privacy Policy</a><script type="text/javascript">(function (w,d) {var loader = function () {var s = d.createElement("script"), tag = d.getElementsByTagName("script")[0]; s.src="https://cdn.iubenda.com/iubenda.js"; tag.parentNode.insertBefore(s,tag);}; if(w.addEventListener){w.addEventListener("load", loader, false);}else if(w.attachEvent){w.attachEvent("onload", loader);}else{w.onload = loader;}})(window, document);</script>
+	</div>
   </footer>
 <!-- </div> -->
 </body>
